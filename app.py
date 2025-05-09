e73fff89fb627d9579c70e5f99489627af9265f0
Bhavana <138198522+BHAVANAKADAVERU@users.noreply.github.com>
2025-05-09 12:11:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ model = joblib.load('diabetes_model.pkl')  # Load the ML model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     results = db.relationship('RiskRecord', backref='user', lazy=True)
 
 class RiskRecord(db.Model):
